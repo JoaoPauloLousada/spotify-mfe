589b1545fd3113b6b565f6c6fd6f070b2778828b
Joao Lousada <joao.lousada@crosslend.com>
2021-12-23 19:37:40 +0100

    [shell] add login app

--- packages/shell/modulefederation.config.js
+++ packages/shell/modulefederation.config.js
@@ -5,7 +5,8 @@ module.exports = {
   filename: "remoteEntry.js",
   remotes: {
     sidebar: "sidebar@http://localhost:8081/remoteEntry.js",
-    player: "player@http://localhost:8082/remoteEntry.js"
+    player: "player@http://localhost:8082/remoteEntry.js",
+    signin: "signin@http://localhost:8083/remoteEntry.js",
   },
   shared: {
     ...dependencies,
--- packages/shell/src/components/Router.js
+++ packages/shell/src/components/Router.js
@@ -1,14 +1,20 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "../views/Home";
-import Login from "../views/Login";
+import ErrorBoundary from "./Error";
+const Login = React.lazy(() => import('signin/Login'))
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <Suspense fallback={<>Loading Component..</>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   )
 }
