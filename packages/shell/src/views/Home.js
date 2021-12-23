112ecf9194fcc4353be51476aa1596831babe45e
Joao Lousada <joao.lousada@crosslend.com>
2021-12-23 19:48:59 +0100

    [shell] update home

--- packages/shell/src/views/Home.js
+++ packages/shell/src/views/Home.js
@@ -14,24 +14,24 @@ function Home() {
 
   return (
     <div className="App">
-      <div className='h-screen w-screen flex flex-col bg-yellow-700'>
-        <div className='grid grid-cols-12 flex-grow'>
-          <div className='bg-yellow-300 col-span-2'>
+      <div className='h-screen w-screen flex flex-col bg-black text-white'>
+        <div className='grid grid-cols-12 flex-grow h-full overflow-hidden'>
+          <div className='col-span-4 lg:col-span-3 xl:col-span-2'>
             <ErrorBoundary>
               <Suspense fallback={<></>}>
                 <Sidebar />
               </Suspense>
             </ErrorBoundary>
           </div>
-          <div className='bg-blue-300 col-span-8'>
+          <div className='col-span-4 lg:col-span-6 xl:col-span-8'>
             content [shell]
           </div>
-          <div className='bg-green-200 col-span-2'>
+          <div className='col-span-4 lg:col-span-3 xl:col-span-2'>
             activity area [shell]
           </div>
 
         </div>
-        <div className='bg-purple-200 col-span-12 h-20'>
+        <div className='col-span-12 h-20'>
           footer [shell]
           <ErrorBoundary>
             <Suspense fallback={<></>}>
