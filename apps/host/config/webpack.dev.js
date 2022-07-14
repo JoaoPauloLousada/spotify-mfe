bc2a37762db80c9ccaade2d54e498098f55c84c1
Joao Lousada <joao.lousada@crosslend.com>
2022-07-14 22:19:14 +0200

    Adding Module Federation Configuration to Host app

--- apps/host/config/webpack.dev.js
+++ apps/host/config/webpack.dev.js
@@ -1,6 +1,9 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+
+
 
 const devConfig = {
   mode: 'development',
@@ -11,6 +14,13 @@ const devConfig = {
     }
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'host',
+      filename: 'remoteEntry.js',
+      remotes: {
+        sidebar: 'sidebar@http://localhost:8081/remoteEntry.js'
+      }
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
