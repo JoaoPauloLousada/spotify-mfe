9c2a73c64e3cff271d259f69eeae50f6b8a32506
Joao Lousada <joao.lousada@crosslend.com>
2022-07-15 20:54:23 +0200

    SIDEBAR: Creating Dependencies context

--- apps/sidebar/src/App.js
+++ apps/sidebar/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Dependencies } from './components/Dependencies'
 import Sidebar from './components/Sidebar'
 
-export default function App() {
-  return <><Sidebar /></>
+export default function App({ spotify }) {
+
+  return (
+    <Dependencies.Provider value={{ spotify }}>
+      <Sidebar />
+    </Dependencies.Provider>
+  )
 }
