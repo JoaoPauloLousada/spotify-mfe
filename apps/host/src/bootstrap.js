94f3a5c518ee1afb1565e6c97f92ee01c54988ce
Joao Lousada <joao.lousada@crosslend.com>
2022-07-15 11:50:18 +0200

    Add react-router to host app

--- apps/host/src/bootstrap.js
+++ apps/host/src/bootstrap.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from "react-router-dom";
 import App from './App'
 
-
 const htmlElement = document.getElementById('host-app')
-ReactDOM.render(<App />, htmlElement)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+  , htmlElement)
