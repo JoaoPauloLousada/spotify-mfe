522ff5f21c00ee9158e952f0f3c06a1685c7e6eb
Joao Lousada <joao.lousada@crosslend.com>
2022-07-15 20:22:54 +0200

    SIDEBAR: Add Shell to sidebar app

--- apps/sidebar/src/bootstrap.js
+++ apps/sidebar/src/bootstrap.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Shell from '@spotify-mfe/shell';
 import App from './App'
 
 
-const mountApplication = htmlElement => {
-  ReactDOM.render(<App />, htmlElement)
+const mountApplication = (htmlElement, dependencies) => {
+  ReactDOM.render(<App {...dependencies} />, htmlElement)
 }
 
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 if (isDevelopment) {
-  const devRootHtmlElement = document.getElementById('side-bar-app')
+  Shell.initialize({ appUrl: 'http://localhost:8081' })
+    .then(({ spotify }) => {
+      const devRootHtmlElement = document.getElementById('side-bar-app')
 
-  if (devRootHtmlElement) mountApplication(devRootHtmlElement)
+      if (devRootHtmlElement) mountApplication(devRootHtmlElement, { spotify })
+    })
+
 }
 
 export { mountApplication }
