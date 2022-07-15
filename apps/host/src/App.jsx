b1f54c1f896fa51190bbf24733141c075a8634e7
Joao Lousada <joao.lousada@crosslend.com>
2022-07-15 11:50:18 +0200

    Add react-router to host app

--- apps/host/src/App.jsx
+++ apps/host/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { mountApplication } from 'sidebar/bootstrap'
-import Application from './components/Application'
+import Router from './Router'
 
 
 export default function App() {
   return <>
     <div>Host App</div>
+    <Router />
     <Application mountApplication={mountApplication} />
   </>
 }
