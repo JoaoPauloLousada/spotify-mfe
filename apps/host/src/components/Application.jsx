9df7f8a3d7081713ce7213e59633fd85df4ec34d
Joao Lousada <joao.lousada@crosslend.com>
2022-07-18 11:36:16 +0200

    HOST: Loading sidebar application within host

--- apps/host/src/components/Application.jsx
+++ apps/host/src/components/Application.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { Dependencies } from '../bootstrap';
 
 export default function Application({ mountApplication }) {
   const htmlElement = useRef(null);
+  const {spotify} = useContext(Dependencies);
 
-  useEffect(() => mountApplication(htmlElement.current), [])
+  useEffect(() => mountApplication(htmlElement.current, {spotify}), [])
 
-  return <div ref={htmlElement}></div>
+  return <div style={{height: '100%'}} ref={htmlElement}></div>
 }
