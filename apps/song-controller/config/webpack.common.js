fe8dbeaaa157adfab68c93e6027f3f4ba6824f8c
Joao Lousada <joao.lousada@crosslend.com>
2022-07-18 21:06:44 +0200

    SONG: Create song controller app

---
+++ apps/song-controller/config/webpack.common.js
@@ -0,0 +1,31 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  }
+}
