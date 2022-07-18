80f71984b3b7d6ab0f99b54c918a3834b7d7dbce
Joao Lousada <joao.lousada@crosslend.com>
2022-07-18 11:25:01 +0200

    SIDEBAR: Finishing styling sidebar

--- apps/sidebar/src/components/Sidebar.jsx
+++ apps/sidebar/src/components/Sidebar.jsx
@@ -1,17 +1,61 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { Dependencies } from './Dependencies'
+import home from '../assets/home.svg'
+import search from '../assets/search.svg'
 
 const Wrapper = styled.div`
   background-color: #000505;
   height:100%;
   width: 100%;
+  color: white;
+  padding: 2rem 1rem;
+  box-sizing: border-box;
+
   * {
     margin: 0;
     padding: 0;
+    font-family: 'Verdana';
+    box-sizing: border-box;
   }
-  color: white;
 `
+
+const List = styled.ul`
+  list-style: none;
+`
+const ListItem = styled.li`
+  min-height: 1.75rem;
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  color: #c7c5c5;
+  transition: color 350ms;
+  margin-top: ${({gap}) => gap && '0.75rem'};
+
+  &:hover {
+    color: white;
+  }
+  
+  > a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    text-decoration: none; 
+    color: inherit
+  }
+`
+
+const Icon = styled.img`
+  display: inline-block;
+  margin-right: 0.75rem;
+  width: 1.25rem;
+  height: 1.25rem;
+`
+
+const Divider = styled.hr`
+  margin: 1.125rem 0;
+`
+
 
 function useUserPlaylists() {
   const { spotify } = useContext(Dependencies);
@@ -30,13 +74,22 @@ export default function Sidebar() {
   const { playlists } = useUserPlaylists()
 
   return <Wrapper>
-    <ul>
-      <li>Home</li>
-      <li>Search</li>
-    </ul>
-    <hr />
-    <ul>
-      {playlists?.map(item => <li key={item.id}>{item.name}</li>)}
-    </ul>
+    <List>
+      <ListItem>
+        <Icon src={home} />
+        <a href="#">Home</a>
+      </ListItem>
+      <ListItem gap>
+        <Icon src={search} />
+        <a href="#">Search</a>
+      </ListItem>
+    </List>
+    <Divider />
+    <List>
+      {playlists?.map(item => (
+      <ListItem key={item.id}>
+        <a href="#">{item.name}</a>
+      </ListItem>))}
+    </List>
   </Wrapper>
 }
