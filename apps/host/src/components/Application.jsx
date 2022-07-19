5c92cf0dec6cbe8d54bff38d51bb1edbfc3cd35f
Joao Lousada <joao.lousada@crosslend.com>
2022-07-19 21:53:14 +0200

    HOST: Create routing listeners

--- apps/host/src/components/Application.jsx
+++ apps/host/src/components/Application.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import { Dependencies } from '../bootstrap';
 
-export default function Application({ mountApplication }) {
+export default function Application({ mountApplication, onMounted }) {
   const htmlElement = useRef(null);
   const {spotify, eventBus} = useContext(Dependencies);
 
-  useEffect(() => mountApplication(htmlElement.current, {spotify, eventBus}), [])
+  useEffect(() => {
+    mountApplication(htmlElement.current, {spotify, eventBus, onMounted})
+  }, [])
 
   return <div style={{height: '100%'}} ref={htmlElement}></div>
 }
