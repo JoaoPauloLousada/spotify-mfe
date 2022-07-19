02cb61a8fc745fb6970b8daa702370a0339ae3fc
Joao Lousada <joao.lousada@crosslend.com>
2022-07-19 21:53:14 +0200

    HOST: Create routing listeners

--- apps/host/src/Router.jsx
+++ apps/host/src/Router.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import { Route, Switch } from "react-router-dom";
+import React, { useCallback, useContext, useEffect } from 'react'
+import { Route, Switch, useHistory } from "react-router-dom";
+import { Dependencies } from './bootstrap';
 import Home from './pages/home';
 
 
 export default function Router() {
+  const { eventBus } = useContext(Dependencies)
+  const history = useHistory();
+  
+  const push = useCallback((appName, route) => {
+    const routePrefix = new Map([
+      ['HOME', '']
+    ])
+
+    if (routePrefix.has(appName)) {
+      const nextPath = `${routePrefix.get(appName)}${route}`
+      if (history.location.pathname !== nextPath) {
+        history.push(nextPath)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    const callback = (event) => {
+      console.log('CHILD_APP:NAVIGATE', event.detail.location.pathname);
+      push(event.detail.appName, event.detail.location.pathname)
+    }
+
+    eventBus.on('CHILD_APP:NAVIGATE', callback);
+
+    history.listen((location) => eventBus.dispatch('PARENT_APP:NAVIGATE', location))
+
+    return () => eventBus.remove('CHILD_APP:NAVIGATE', callback)
+  }, [])
+
   return (
     <Switch>
       <Route path="/">
--- apps/host/src/components/HomeWrapper.jsx
+++ apps/host/src/components/HomeWrapper.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import { mountApplication } from 'home/bootstrap'
 import Application from './Application';
+import { useHistory } from 'react-router-dom';
+import { Dependencies } from '../bootstrap';
 
 
 export default function HomeWrapper() {
+  const history = useHistory();
+  const {eventBus} = useContext(Dependencies)
+  const onMounted = useCallback(() => {
+    eventBus.dispatch('PARENT_APP:NAVIGATE', history.location)
+  }, [])
   return (
-    <Application mountApplication={mountApplication} />
+    <Application mountApplication={mountApplication} onMounted={onMounted} />
   )
 }
