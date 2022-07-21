d2b8ddf70b93d9106792c05bf20b7f84cb4ca94b
Joao Lousada <joao.lousada@crosslend.com>
2022-07-21 20:52:19 +0200

    update BASE_URL

--- libs/shell/index.js
+++ libs/shell/index.js
@@ -2,12 +2,15 @@ import SpotifyAPI from 'spotify-web-api-js';
 import Auth from './src/auth'
 import EventBus from './src/EventEmitter';
 
+const BASE_URL = process.env.BASE_URL
+
 const initialize = async ({ appUrl }) => {
+  const url = BASE_URL || appUrl
   const spotify = new SpotifyAPI()
-  const auth = new Auth({ appUrl, spotify })
+  const auth = new Auth({ appUrl: url, spotify })
 
   if (!await auth.handleAuth()) {
-    console.error('User is NOT authenticated', appUrl)
+    console.error('User is NOT authenticated', url)
     return Promise.reject({})
   }
 
