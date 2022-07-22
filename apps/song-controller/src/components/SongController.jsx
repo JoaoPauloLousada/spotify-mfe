d0f3083594388f8be1e8ca8c420c2dc01fd12bf0
Joao Lousada <joao.lousada@crosslend.com>
2022-07-22 14:23:43 +0200

    SONG: Add play and pause features

--- apps/song-controller/src/components/SongController.jsx
+++ apps/song-controller/src/components/SongController.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Dependencies } from '../App'
 import play from '../assets/play.svg'
+import pause from '../assets/pause.svg'
 import playForward from '../assets/play-forward.svg'
 import sound from '../assets/sound.svg'
 import Range from './Range'
@@ -47,11 +48,28 @@ const Right = styled.div`
 export default function SongController() {
   const {spotify, eventBus} = useContext(Dependencies)
   const [track, setTrack] = useState(null);
+  const [playIcon, setPlayIcon] = useState(play)
 
   useEffect(() => {
-    const callback = (e) => {
-      spotify.play({context_uri: e.detail.uri})
-      setTrack(e.detail)
+    const checkPlaybackState = async () => {
+      const playbackState = await spotify.getMyCurrentPlaybackState()
+  
+      if (playbackState.is_playing) setPlayIcon(pause)
+      checkTrack()
+    }
+
+    checkPlaybackState()
+
+    const callback = async (e) => {
+      try {
+        await spotify.play({context_uri: e.detail.album.uri, offset: { position: e.detail.track_number - 1 }})
+        setTrack(e.detail)
+      } catch (err) {
+        const error = JSON.parse(err.response)?.error
+        if (error?.reason && error.reason == "NO_ACTIVE_DEVICE") {
+          alert('You have no active device. Please try to open your spotify app in one of your devices.')
+        }
+      }
     }
     
     eventBus.on('PLAY', callback)
@@ -60,6 +78,43 @@ export default function SongController() {
       eventBus.remove('PLAY', callback)
     }
   }, [])
+
+  const checkTrack = async () => {
+    const playbackState = await spotify.getMyCurrentPlaybackState()
+    if (playbackState.item) setTrack(playbackState.item)
+  } 
+
+  const playOrPause = async () => {
+    const playbackState = await spotify.getMyCurrentPlaybackState()
+    try {
+      if (playbackState.is_playing) {
+        await spotify.pause()
+        setPlayIcon(play)
+        checkTrack()
+      } else {
+        await spotify.play()
+        setPlayIcon(pause)
+        checkTrack()
+      }
+    } catch (err) {
+      const error = JSON.parse(err.response)?.error
+      if (error?.reason && error.reason == "NO_ACTIVE_DEVICE") {
+        alert('You have no active device. Please try to open your spotify app in one of your devices.')
+      }
+    }
+  }
+
+  const skipToNext = async () => {
+    await spotify.skipToNext()
+    setPlayIcon(pause)
+    setTimeout(checkTrack, 1000)
+  }
+
+  const skipToPrevious = async () =>  {
+    await spotify.skipToPrevious()
+    setPlayIcon(pause)
+    setTimeout(checkTrack, 1000)
+  }
 
   return (
     <Wrapper>
@@ -73,19 +128,19 @@ export default function SongController() {
       </Left>
       <Center>
         <div>
-          <img src={playForward} width="14" />
-          <img src={play} width="28" style={{margin: '0 1rem'}}/>
-          <img src={playForward} width="14" />
+          <img src={playForward} width="14" onClick={skipToPrevious} style={{transform: 'rotate(180deg)'}} />
+          <img src={playIcon} width="28" style={{margin: '0 1rem'}} onClick={playOrPause}/>
+          <img src={playForward} width="14" onClick={skipToNext} />
         </div>
-        <div style={{marginTop: '1rem'}}>
+        {/* <div style={{marginTop: '1rem'}}>
           <Range />
-        </div>
+        </div> */}
       </Center>
       <Right>
-        <img src={sound} width="18" />
+        {/* <img src={sound} width="18" />
         <div style={{width: '120px', marginLeft: '1rem'}}>
           <Range />
-        </div>
+        </div> */}
       </Right>
     </Wrapper>
   )
