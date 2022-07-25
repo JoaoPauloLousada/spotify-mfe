8d85e92a4ede3a32cb0809d242ae497c4c59dcfb
Joao Lousada <joao.lousada@crosslend.com>
2022-07-25 15:17:55 +0200

    SEARCH: Add theme to search

--- libs/shell/src/theme/index.js
+++ libs/shell/src/theme/index.js
@@ -3,7 +3,7 @@ export default {
   'gray-200': '#535353',
   'gray-250': '#2A2A2A',
   'gray-300': '#212121',
-  'dark': '#121212',
+  'dark': '#080808',
   'primary': '#1db954',
   'light': '#fff'
 }
